bbd1a37d8e421bc27bfddea401040997b4eb649e
IvanDovhopol <104319381+IvanDovhopol@users.noreply.github.com>
2022-10-03 17:18:58 +0000

    Deploying to gh-pages from @ IvanDovhopol/parcel-st@92e0866d66d4856c9bb410bdd8b6aad8e11c0527 🚀

--- promise.150a4688.js
+++
@@ -1,2 +0,0 @@
-!function(){var o;(o=4,fetch("https://pokeapi.co/api/v2/pokemon/".concat(o)).then((function(o){return o.json()}))).then((function(o){console.log("pokemon => ",o)})).catch((function(o){console.error("ошибка в блоке catch",o)}))}();
-//# sourceMappingURL=promise.150a4688.js.map
--- promise.a509de84.js
+++
@@ -1,2 +0,0 @@
-var o;(o=4,fetch(`https://pokeapi.co/api/v2/pokemon/${o}`).then((o=>o.json()))).then((function(o){console.log("pokemon => ",o)})).catch((function(o){console.error("ошибка в блоке catch",o)}));
-//# sourceMappingURL=promise.a509de84.js.map
